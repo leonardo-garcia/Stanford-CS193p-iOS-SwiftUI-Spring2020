ef0c4a7a142fe8a9221c3bd68d7292c179a5ea42
Leonardo Garcia <leo.equih@gmail.com>
2020-05-31 13:03:07 -0500

    Draw CardView
    Finished Stanford CS193p lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,7 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack(spacing: 16) {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+            
+        .foregroundColor(Color.orange)
+        .padding()
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻").font(Font.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+
+            }
+        }
     }
 }
 
